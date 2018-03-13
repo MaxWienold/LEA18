61609b3631501686f6dddb8e22c1679e599c229c
Max Wienold <max.wienold@email.de>
2018-03-13 21:27:53 +0100

    neue Klasse Funktionen

---
+++ src/Funktionen.java
@@ -0,0 +1,21 @@
+
+
+public class Funktionen {
+	
+	public static void erklärung() {
+
+		
+	}
+	public static void lateinisch() {
+		
+	}
+	
+	public static void arabisch() {
+		
+	}
+	
+	public static void dateiFormatieren() {
+		
+	}
+
+}
