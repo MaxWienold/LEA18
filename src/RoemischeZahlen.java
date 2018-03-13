791e69964ba7207a1ede47462637fce65b920f79
Max Wienold <max.wienold@email.de>
2018-03-13 21:32:18 +0100

    Begrüßungsnachricht in der main

--- src/RoemischeZahlen.java
+++ src/RoemischeZahlen.java
@@ -3,10 +3,36 @@ import java.util.Scanner;
 public class RoemischeZahlen {
 
 	public static Scanner eingabe = new Scanner(System.in);
+	
+	
 
 	public static void main(String[] args) {
+	    System.out.println("       ____            ____\n"
+	    		+"      /   /           /   /\n"
+	    		+"     /   /           /   /\n"
+	    		+"    /   /___   ____ /   /___\n"
+	    		+"   /   _    \\ /___//   _    \\\n"
+	    		+"  /   / /   //   //   / /   /\n"
+	    		+" /   /_/   //   //   /_/   / ____ ____\n"
+	    		+"/         //   //         / /   //   /\n"
+	    		+"\\________/ \\__/ \\________/ /   //   /\n"
+	    		+"    ________   ________   /   //   /________   ________   ________\n"
+	    		+"   /   __   \\ /   _    \\ /   //   //   ___  \\ /   __   \\ /   ___  \\\n"
+	    		+"  /   / /___//   / /   //   //   //   /__/  //   / /___//   /__/  /\n"
+	    		+" /   /___   /   /_/   //   //   //   ______//   /_____ /   ______/\n"
+	    		+"/        \\ /         //   //   //         //         //         /\n"
+	    		+"\\________/ \\________/ \\___\\\\___\\\\________/ \\____/___/ \\________/\n");
+
+	    
+	    System.out.println("____________________________\n\n"
+	    		+ "lateinischer Rechner\n\n"
+	    		+ "entstanden als LEA im Frühjahr 2018\n"
+	    		+ "Entwickelt von Volkan Hanke und Max Wienold\n"
+	    		+ "____________________________\n\n");
+	    
+	    
 		Menue.arrayAufbauen();
-		Ausgaben.menuAnzeigen();
+		Menue.schleife();
 		
 		
 
